6dd98bae10ada09b37a572c3a7a7dc2dea3d56ea
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-20 16:28:13 -0500

    Initial Commit

---
+++ FinalCalorieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalCalorieApp
+//
+//  Created by student on 3/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
