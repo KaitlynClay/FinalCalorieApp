b5c22a4130cf26ee5534b97a08bdc2ba18f8c308
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-23 16:18:19 -0500

    ui works

---
+++ FinalCalorieApp/CalJournalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalJournalViewController.swift
+//  FinalCalorieApp
+//
+//  Created by student on 3/23/24.
+//
+
+import UIKit
+
+class CalJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToCalJournal(_ sender: UIStoryboardSegue) {}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
